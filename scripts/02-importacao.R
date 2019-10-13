21b72d2a088f4f8efff6583a7ac43196441d1216
Athos Damiani <athos.damiani@gmail.com>
2019-10-13 20:23:52 -0300

    write_*() e detalhes

--- scripts/02-importacao.R
+++ scripts/02-importacao.R
@@ -1,4 +1,3 @@
-
 library(tidyverse)
 
 # Caminhos até o arquivo --------------------------------------------------
@@ -9,6 +8,9 @@ library(tidyverse)
 # 2. Podem ser relativos ao diretório de trabalho
 getwd()
 
+# exercício: na linha debaixo, abram aspas e apertem TAB.
+
+# LEITURA DE DADOS --------------------------------------------------------
 # Principais formatos -----------------------------------------------------
 
 # Arquivos de texto
@@ -17,10 +19,14 @@ imdb2 <- read_delim("dados/imdb2.csv", delim = ";")
 
 # Excel
 library(readxl)
-imdb_excel <- read_excel("dados/imdb.xlsx")
+imdb_excel <- read_excel("dados/imdb.xlsx", sheet = 1:2)
+
+meu_bd <- read_fwf(file = "dados/imdb.csv", col_positions = c(3, 6, 10, 100))
+names(meu_bd) <- c("coluna 1", "preco", "nome do cliente", "data")
+
 
 # SQL ---------------------------------------------------------------------
-
+install.packages("RSQLite")
 conexao <- src_sqlite("dados/imdb.sqlite", create = TRUE)
 # copy_to(conexao, imdb, temporary = FALSE)
 
@@ -30,6 +36,8 @@ imdb_select <- tbl(conexao, sql("SELECT titulo, ano, diretor FROM imdb"))
 # trazer para a memória
 collect(imdb_sqlite)
 collect(imdb_select)
+DBI::dbSendQuery(conexao, "use meuSchema")
+copy_to(conexao, imdb)
 
 # db.rstudio.com
 
@@ -43,10 +51,16 @@ library(haven)
 imdb_sas <- read_sas("dados/imdb.sas7bdat")
 imdb_spss <- read_spss("dados/imdb.sav")
 
-# pacote rio
-library(rio)
-imdb_rio <- rio::import("dados/imdb.xlsx")
-
 # data.table
 library(data.table)
 imdb_dt <- fread("dados/imdb.csv")
+
+
+# GRAVANDO DADOS ------------------------------------------------------------
+# funcoes que iniciam com 'write'
+# csv
+write_csv(imdb, path = "dados/imdb.csv")
+
+# Excel
+library(writexl)
+write_xlsx(imdb_excel, path = "dados/imdb.xlsx")
