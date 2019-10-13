21f3618cd5fd1dc8ee8be48b8d1383d07cebe6b9
Athos Damiani <athos.damiani@gmail.com>
2019-10-13 20:26:56 -0300

    dicas rapidas

---
+++ scripts/dicas_rapidas.R
@@ -0,0 +1,89 @@
+
+## Dicas rápidas! -----------------------------
+
+# qual a função que faz/constrói vetores?
+
+
+
+# qual a diferença entre as duas partes abaixo?
+
+# parte 1
+x <- 1:5
+
+# parte 2
+y <- c(1,2,3,4,5)
+
+
+
+# qual a diferença entre as duas partes abaixo?
+
+# parte 1
+x <- 1:5
+sum(x)
+
+# parte 2
+sum(c(1,2,3,4,5))
+
+
+
+
+
+# qual a diferença entre as duas partes abaixo?
+x <- 1:5
+
+# parte 1
+x[x > 3]
+
+# parte 2
+x[c(FALSE, FALSE, FALSE, TRUE, TRUE)]
+
+
+# qual a diferença entre as duas partes abaixo?
+x <- 1:5
+
+# parte 1
+sin(x)
+
+# parte 2
+x %>% sin
+
+
+
+
+
+
+
+
+
+# qual a diferença entre as duas partes abaixo?
+
+# parte 1
+sin(c(1,2,3,4,5))
+
+# parte 2
+sin(as.numeric(c("1","2","3","4","5")))
+
+
+
+# descubra quais pastas estão na sua 
+# área de trabalho sem sair daqui!
+# DICA: **abra e feche aspas**
+
+
+
+# Qual a diferença entre apertar CTRL+ENTER 
+# sem selecionar nada VERSUS selecionando
+# uma linha inteira?
+
+
+
+# Utilizando o tab, o nome do pacote e o '::', 
+# faça o rstudio listar todas as funções do
+# pacote stringr.
+
+
+
+# o x é um vetor pequeno, então qual seria 
+# uma maneira simples e direta de ver o que tem nele?
+
+
