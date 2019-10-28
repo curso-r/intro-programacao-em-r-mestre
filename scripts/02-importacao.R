7efda5462c04461d1d314b233f989722a7132ddb
williamorim <thewilliam89@gmail.com>
2019-10-28 12:35:03 -0300

    tirando duplicatas

--- scripts/02-importacao.R
+++ scripts/02-importacao.R
@@ -8,25 +8,25 @@ library(tidyverse)
 # 2. Podem ser relativos ao diretório de trabalho
 getwd()
 
-# exercício: na linha debaixo, abram aspas e apertem TAB.
-
-# LEITURA DE DADOS --------------------------------------------------------
-# Principais formatos -----------------------------------------------------
+# Leitura de dados --------------------------------------------------------
 
 # Arquivos de texto
 imdb <- read_csv(file = "dados/imdb.csv")
-imdb2 <- read_delim("dados/imdb2.csv", delim = ";")
+imdb2 <- read_csv2(file = "dados/imdb2.csv")
+
+imdb2 <- read_delim("dados/imdb.csv", delim = ",")
+
+
 
 # Excel
 library(readxl)
-imdb_excel <- read_excel("dados/imdb.xlsx", sheet = 1:2)
-
-meu_bd <- read_fwf(file = "dados/imdb.csv", col_positions = c(3, 6, 10, 100))
-names(meu_bd) <- c("coluna 1", "preco", "nome do cliente", "data")
+imdb_excel <- read_excel("dados/imdb.xlsx")
 
 
-# SQL ---------------------------------------------------------------------
-install.packages("RSQLite")
+
+# SQL
+# install.packages("RSQLite")
+
 conexao <- src_sqlite("dados/imdb.sqlite", create = TRUE)
 # copy_to(conexao, imdb, temporary = FALSE)
 
@@ -36,12 +36,12 @@ imdb_select <- tbl(conexao, sql("SELECT titulo, ano, diretor FROM imdb"))
 # trazer para a memória
 collect(imdb_sqlite)
 collect(imdb_select)
-DBI::dbSendQuery(conexao, "use meuSchema")
-copy_to(conexao, imdb)
 
-# db.rstudio.com
+# Mais informações: db.rstudio.com
 
-# Outros formatos ---------------------------------------------------------
+
+
+# Outros formatos
 
 library(jsonlite)
 imdb_json <- read_json("dados/imdb.json")
@@ -51,16 +51,17 @@ library(haven)
 imdb_sas <- read_sas("dados/imdb.sas7bdat")
 imdb_spss <- read_spss("dados/imdb.sav")
 
-# data.table
-library(data.table)
-imdb_dt <- fread("dados/imdb.csv")
 
+# Gravando dados------------------------------------------------------------
 
-# GRAVANDO DADOS ------------------------------------------------------------
-# funcoes que iniciam com 'write'
+# As funções iniciam com 'write'
+
 # csv
-write_csv(imdb, path = "dados/imdb.csv")
+write_csv(imdb, path = "imdb.csv")
 
 # Excel
 library(writexl)
-write_xlsx(imdb_excel, path = "dados/imdb.xlsx")
+write_xlsx(imdb, path = "imdb.xlsx")
+
+# rds
+write_rds(imdb, path = "imdb.rds")
