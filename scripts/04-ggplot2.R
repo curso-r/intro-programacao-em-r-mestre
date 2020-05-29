b725e4f33371e9ea006502c8f71ef71d8ec2ad5b
williamorim <thewilliam89@gmail.com>
2020-05-29 19:55:56 -0300

    aula ggplot2

--- scripts/04-ggplot2.R
+++ scripts/04-ggplot2.R
@@ -23,8 +23,17 @@ imdb <- imdb %>% mutate(lucro = receita - orcamento)
 
 # Gráfico de pontos (dispersão) -------------------------------------------
 
+# Apenas o canvas
+imdb %>% 
+  ggplot()
+
+# Salvando em um objeto
+p <- imdb %>% 
+  ggplot()
+
 # Gráfico de dispersão da receita contra o orçamento
-ggplot(imdb) +
+imdb %>% 
+  ggplot() +
   geom_point(mapping = aes(x = orcamento, y = receita))
 
 # Inserindo a reta x = y
@@ -117,6 +126,10 @@ imdb %>%
   geom_label(aes(label = nota_media))
 
 
+# Exercício ---------------------------------------------------------------
+
+# Faça um gráfico do orçamento médio dos filmes ao longo dos anos.
+
 # Gráfico de barras -------------------------------------------------------
 
 # Número de filmes dos diretores da base
@@ -186,13 +199,156 @@ imdb %>%
   coord_flip()
 
 
+# Exercícios --------------------------------------------------------------
+
+# a. Transforme o gráfico do exercício anterior em um gráfico de barras.
+
+# b. Refaça o gráfico apenas para filmes de 1989 para cá.
+
 # Histogramas e boxplots --------------------------------------------------
 
+# Histograma do lucro dos filmes do Steven Spielberg 
+imdb %>% 
+  filter(diretor == "Steven Spielberg") %>%
+  ggplot() +
+  geom_histogram(aes(x = lucro))
 
+# Arrumando o tamanho das bases
+imdb %>% 
+  filter(diretor == "Steven Spielberg") %>%
+  ggplot() +
+  geom_histogram(
+    aes(x = lucro), 
+    binwidth = 100000000,
+    color = "white"
+  )
+
+# Boxplot do lucro dos filmes dos diretores
+# fizeram mais de 15 filmes
+imdb %>% 
+  filter(!is.na(diretor)) %>%
+  group_by(diretor) %>% 
+  filter(n() >= 15) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = diretor, y = lucro))
+
+# Ordenando pela mediana
+
+imdb %>% 
+  filter(!is.na(diretor)) %>%
+  group_by(diretor) %>% 
+  filter(n() >= 15) %>% 
+  ungroup() %>% 
+  mutate(diretor = forcats::fct_reorder(diretor, lucro, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_boxplot(aes(x = diretor, y = lucro))
+
+
+# Exercícios --------------------------------------------------------------
+
+#a. Descubra quais são os 5 atores que mais aparecem na coluna ator_1.
+
+#b. Faça um boxplot do lucro dos filmes desses atores.
 
 # Título e labels ---------------------------------------------------------
 
+# Labels
+imdb %>%
+  ggplot() +
+  geom_point(mapping = aes(x = orcamento, y = receita, color = lucro)) +
+  labs(
+    x = "Orçamento ($)",
+    y = "Receita ($)",
+    color = "Lucro ($)",
+    title = "Gráfico de dispersão",
+    subtitle = "Receita vs Orçamento"
+  )
 
+# Escalas
+imdb %>% 
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = ano, y = nota_media)) +
+  scale_x_continuous(breaks = seq(1916, 2016, 10)) +
+  scale_y_continuous(breaks = seq(0, 10, 2))
 
+# Visão do gráfico
+imdb %>% 
+  group_by(ano) %>% 
+  summarise(nota_media = mean(nota_imdb, na.rm = TRUE)) %>% 
+  ggplot() +
+  geom_line(aes(x = ano, y = nota_media)) +
+  scale_x_continuous(breaks = seq(1916, 2016, 10)) +
+  scale_y_continuous(breaks = seq(0, 10, 2)) +
+  coord_cartesian(ylim = c(0, 10))
 
+# Cores -------------------------------------------------------------------
 
+# Escolhendo cores pelo nome
+imdb %>% 
+  count(diretor) %>%
+  filter(!is.na(diretor)) %>% 
+  top_n(5, n) %>%
+  ggplot() +
+  geom_bar(
+    aes(x = diretor, y = n, fill = diretor), 
+    stat = "identity",
+    show.legend = FALSE
+  ) +
+  coord_flip() +
+  scale_fill_manual(values = c("red", "blue", "green", "pink", "purple"))
+
+# Escolhendo pelo hexadecimal
+imdb %>% 
+  count(diretor) %>%
+  filter(!is.na(diretor)) %>% 
+  top_n(5, n) %>%
+  ggplot() +
+  geom_bar(
+    aes(x = diretor, y = n, fill = diretor), 
+    stat = "identity",
+    show.legend = FALSE
+  ) +
+  coord_flip() +
+  scale_fill_manual(
+    values = c("#ff4500", "#268b07", "#ff7400", "#0befff", "#a4bdba")
+  )
+
+# Mudando textos da legenda
+imdb %>% 
+  filter(!is.na(cor)) %>% 
+  group_by(ano, cor) %>% 
+  summarise(num_filmes = n()) %>% 
+  ggplot() +
+  geom_line(aes(x = ano, y = num_filmes, color = cor)) +
+  scale_color_discrete(labels = c("Preto e branco", "Colorido"))
+
+# Setando cores das formas
+imdb %>% 
+  ggplot() +
+  geom_point(mapping = aes(x = orcamento, y = receita), color = "#ff7400")
+
+# Tema --------------------------------------------------------------------
+
+# Temas prontos
+imdb %>% 
+  ggplot() +
+  geom_point(mapping = aes(x = orcamento, y = receita)) +
+  # theme_bw() 
+  # theme_classic() 
+  # theme_dark()
+  theme_minimal()
+
+# A função theme()
+imdb %>% 
+  ggplot() +
+  geom_point(mapping = aes(x = orcamento, y = receita)) +
+  labs(
+    title = "Gráfico de dispersão",
+    subtitle = "Receita vs Orçamento"
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5)
+  )
